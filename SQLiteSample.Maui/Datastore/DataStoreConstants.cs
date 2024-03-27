2c9a9b7603b94e4ee8f39a3115055eb0dd6b6e13
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2024-03-27 15:19:55 +0530

    fix: clean up

--- SQLiteSample.Maui/Datastore/DataStoreConstants.cs
+++ SQLiteSample.Maui/Datastore/DataStoreConstants.cs
@@ -2,5 +2,6 @@
 
 public static class DataStoreConstants
 {
+    // you can change this name based on whatever you need
     public static readonly string DatabaseName = "sqlite.db3";
 }
--- SQLiteSample.Maui/GroceryModel.cs
+++ SQLiteSample.Maui/GroceryModel.cs
@@ -1,4 +1,5 @@
 ﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace SQLiteSample.Maui;
 
@@ -6,5 +7,17 @@ public class GroceryModel
 {
     [PrimaryKey,AutoIncrement]
     public int Id { get; set; }
+
     public string? Name { get; set; }
+
+    [OneToMany(CascadeOperations = CascadeOperation.All)]
+    public List<string> OneToManyAll { get; set; }
+      = new List<string>
+        {
+            "This is some random data",
+            "To show how relational cascading would be done",
+            "for a basic primitive type",
+            "Its the same for non primitive types too",
+            "This data is hardcoded but can be added from your DTOs as well obviously"
+        };
 }
